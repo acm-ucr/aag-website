34f8c33238743d5955533bb2956127373f864a93
Atharva <aneva018@ucr.edu>
2023-10-17 15:42:20 -0700

    formatted

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
-
 import ImageBars from "@/components/ImageBars";
 const Home = () => {
-  return(
-
+  return (
     <div>
-     <div className="text-white ">Home</div>
-    <ImageBars/>
+      <div className="text-white ">Home</div>
+      <ImageBars />
     </div>
-
-  );    
+  );
 };
 
 export default Home;
--- src/components/ImageBars.jsx
+++ src/components/ImageBars.jsx
@@ -6,20 +6,32 @@ import image3 from "../../public/image_3.png";
 import image4 from "../../public/image_4.png";
 import image5 from "../../public/image_5.png";
 
-
-
 const ImageBars = () => {
-  
-    return(
-      <div className="flex">
-        <Image src={image1} alt="Blue Dancer" className="m-[0.85%] mb-[7%] mt-[3%]"/>
-        <Image src={image4} alt="3 white pic" className="m-[0.85%] mt-[10%]"/>
-        <Image src={image2} alt="Yellow w/purple background"className="m-[0.85%]  mb-[4%] mt-[6%]"/>
-        <Image src={image5} alt="1 white pic"className="m-[0.85%] mt-[1%] mb-[10%] "/>
-        <Image src={image3} alt="Red Background"className="m-[0.85%] mb-[2%] mt-[8%]"/>        
-      </div>
-    );
-  
+  return (
+    <div className="flex">
+      <Image
+        src={image1}
+        alt="Blue Dancer"
+        className="m-[0.85%] mb-[7%] mt-[3%]"
+      />
+      <Image src={image4} alt="3 white pic" className="m-[0.85%] mt-[10%]" />
+      <Image
+        src={image2}
+        alt="Yellow w/purple background"
+        className="m-[0.85%]  mb-[4%] mt-[6%]"
+      />
+      <Image
+        src={image5}
+        alt="1 white pic"
+        className="m-[0.85%] mt-[1%] mb-[10%] "
+      />
+      <Image
+        src={image3}
+        alt="Red Background"
+        className="m-[0.85%] mb-[2%] mt-[8%]"
+      />
+    </div>
+  );
 };
 
 export default ImageBars;
