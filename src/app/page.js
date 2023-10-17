6731eacfa3f50f8613a6dfa681ac16c653c50fc8
Atharva <aneva018@ucr.edu>
2023-10-17 15:40:42 -0700

    added image bars and renamed pngs and formatted

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
+
+import ImageBars from "@/components/ImageBars";
 const Home = () => {
-  return <div className="text-white">Home</div>;
+  return(
+
+    <div>
+     <div className="text-white ">Home</div>
+    <ImageBars/>
+    </div>
+
+  );    
 };
 
 export default Home;
