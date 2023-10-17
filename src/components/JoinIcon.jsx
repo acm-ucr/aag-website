88801e1057b1ba8386f7c5d9ab60f6ae5aed30ec
low <akilan.purushothaman@gmail.com>
2023-10-16 23:23:06 -0700

    Create JoinIcon Component Fixes #6

--- src/components/JoinIcon.jsx
+++ src/components/JoinIcon.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Link from "next/Link";
 
-const JoinIcon = () => {
-  return <div>JoinIcon</div>;
+const JoinIcon = (props) => {
+  return (
+    <Link
+      className="flex flex-col justify-center items-center no-underline transition ease-in-out hover:opacity-50"
+      href={props.herf}
+    >
+      <img src={props.img} />
+      <h4 className="text-white">{props.name}</h4>
+    </Link>
+  );
 };
 
 export default JoinIcon;
