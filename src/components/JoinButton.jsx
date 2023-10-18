3e36920f60b0db311355ae40db79dbe866bb0e38
Darius Mahamedi <dmaha004@ucr.edu>
2023-10-18 14:11:29 -0700

    Changed colors to aag colors, change opacity

--- src/components/JoinButton.jsx
+++ src/components/JoinButton.jsx
@@ -5,8 +5,8 @@ const JoinButton = () => {
   return (
     <Link href=" https://docs.google.com/forms/d/e/1FAIpQLSd-83k_aUvm_-HMH4LZRpsSVBMDFK8yf3uRygBaFVJHRZQqnw/viewform">
       <button
-        className="bg-gradient-to-r from-orange-800 to-orange-500 hover:bg-none hover:bg-orange-800 text-white font-bold px-4 py-2 
-    rounded-md hover:rounded-lg active:rounded-full"
+        className="bg-gradient-to-r from-aag-red to-aag-orange text-white font-bold px-4 py-2 
+    hover:opacity-80 active:opacity-60"
       >
         JOIN
       </button>
