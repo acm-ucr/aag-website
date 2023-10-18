9bee6df0476d6eaf981f520e6b3eb3e831c91901
Darius Mahamedi <dmaha004@ucr.edu>
2023-10-18 10:43:26 -0700

    added JoinButton component

--- src/components/JoinButton.jsx
+++ src/components/JoinButton.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Link from "next/Link";
 
 const JoinButton = () => {
-  return <div>JoinButton</div>;
+  return (
+    <Link href=" https://docs.google.com/forms/d/e/1FAIpQLSd-83k_aUvm_-HMH4LZRpsSVBMDFK8yf3uRygBaFVJHRZQqnw/viewform">
+      <button
+        className="bg-gradient-to-r from-orange-800 to-orange-500 hover:bg-none hover:bg-orange-800 text-white font-bold px-4 py-2 
+    rounded-md hover:rounded-lg active:rounded-full"
+      >
+        JOIN
+      </button>
+    </Link>
+  );
 };
 
 export default JoinButton;
