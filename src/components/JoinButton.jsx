1dd0ded43e809523d2c99ba334f95a5782a415f4
Shing Hung <123037099+ysh0305@users.noreply.github.com>
2023-10-18 15:24:46 -0700

    Update JoinButton.jsx

--- src/components/JoinButton.jsx
+++ src/components/JoinButton.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Link from "next/Link";
+import Link from "next/link";
 
 const JoinButton = () => {
   return (
     <Link href=" https://docs.google.com/forms/d/e/1FAIpQLSd-83k_aUvm_-HMH4LZRpsSVBMDFK8yf3uRygBaFVJHRZQqnw/viewform">
       <button
-        className="bg-gradient-to-r from-aag-red to-aag-orange text-white font-bold px-4 py-2 
+        className="bg-gradient-to-r from-aag-red to-aag-orange text-2xl text-white font-bold px-5 py-2 
     hover:opacity-80 active:opacity-60"
       >
         JOIN
