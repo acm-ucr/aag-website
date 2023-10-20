cc371b92ddbf5a92ea0b850c87a517d04a3a2665
Shing Hung <123037099+ysh0305@users.noreply.github.com>
2023-10-19 19:59:33 -0700

    Update JoinButton.jsx

--- src/components/JoinButton.jsx
+++ src/components/JoinButton.jsx
@@ -3,13 +3,12 @@ import Link from "next/link";
 
 const JoinButton = () => {
   return (
-    <Link href=" https://docs.google.com/forms/d/e/1FAIpQLSd-83k_aUvm_-HMH4LZRpsSVBMDFK8yf3uRygBaFVJHRZQqnw/viewform">
-      <button
-        className="bg-gradient-to-r from-aag-red to-aag-orange text-2xl text-white font-bold px-5 py-2 
+    <Link
+      className="bg-gradient-to-r from-aag-red to-aag-orange text-2xl text-white font-bold px-5 py-2 
     hover:opacity-80 active:opacity-60"
-      >
-        JOIN
-      </button>
+      href=" https://docs.google.com/forms/d/e/1FAIpQLSd-83k_aUvm_-HMH4LZRpsSVBMDFK8yf3uRygBaFVJHRZQqnw/viewform"
+    >
+      JOIN
     </Link>
   );
 };
