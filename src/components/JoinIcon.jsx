7e2a51a8bf9984b4935076c08ab883dae24eeec0
ysh0305 <yhung022@ucr.edu>
2023-10-19 21:27:10 -0700

    test

--- src/components/JoinIcon.jsx
+++ src/components/JoinIcon.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/Link";
+import Link from "next/link";
 import Image from "next/image";
 import {
   AiOutlineInstagram,
@@ -7,60 +7,74 @@ import {
   AiFillFacebook,
 } from "react-icons/ai";
 import { BiLogoTiktok } from "react-icons/bi";
+
+const iconFormat =
+  "transition ease-in-out hover:text-aag-red text-6xl flex flex-col justify-center items-center space-y-5";
+const textFormat = "text-lg font-light";
+
 const JoinIcon = ({ type }) => {
-  if (type === "gigsalad") {
-    return (
-      <Link
-        className="flex flex-col justify-center items-center space-y-5 no-underline transition ease-in-out hover:opacity-50"
-        href="https://www.gigsalad.com/ucr_aag_riverside"
-      >
-        <Image width="70" height="70" src="logo.svg" />
-        <h5 className="text-white font-extralight">GigSalad</h5>
-      </Link>
-    );
-  }
   if (type === "instagram") {
     return (
-      <Link
-        className="flex flex-col justify-center items-center space-y-5 no-underline transition ease-in-out hover:opacity-50"
-        href="https://www.instagram.com/ucraag"
-      >
-        <AiOutlineInstagram color="white" size="70" />
-        <h5 className="text-white font-extralight">Instagram</h5>
-      </Link>
+      <div className="text-white">
+        <Link className={iconFormat} href="https://www.instagram.com/ucraag">
+          <AiOutlineInstagram />
+          <p className={textFormat}>Instagram</p>
+        </Link>
+      </div>
     );
   }
   if (type === "tiktok") {
     return (
-      <Link
-        className="flex flex-col justify-center items-center space-y-5 no-underline transition ease-in-out hover:opacity-50"
-        href="https://www.tiktok.com/@ucraag"
-      >
-        <BiLogoTiktok color="white" size="70" />
-        <h5 className="text-white font-extralight">Tiktok</h5>
-      </Link>
+      <div className="text-white">
+        <Link className={iconFormat} href="https://www.tiktok.com/@ucraag">
+          <BiLogoTiktok />
+          <p className={textFormat}>Tiktok</p>
+        </Link>
+      </div>
     );
   }
   if (type === "youtube") {
     return (
-      <Link
-        className="flex flex-col justify-center items-center space-y-5 no-underline transition ease-in-out hover:opacity-50"
-        href="https://www.youtube.com/@ucraag/videos"
-      >
-        <AiFillYoutube color="white" size="70" />
-        <h5 className="text-white font-extralight">Youtube</h5>
-      </Link>
+      <div className="text-white">
+        <Link
+          className={iconFormat}
+          href="https://www.youtube.com/@ucraag/videos"
+        >
+          <AiFillYoutube />
+          <p className={textFormat}>Youtube</p>
+        </Link>
+      </div>
     );
   }
   if (type === "facebook") {
     return (
-      <Link
-        className="flex flex-col justify-center items-center space-y-5 no-underline transition ease-in-out hover:opacity-50"
-        href="https://www.facebook.com/profile.php?viewas=100000686899395&id=61550312611527"
-      >
-        <AiFillFacebook color="white" size="70" />
-        <h5 className="text-white font-extralight">Facebook</h5>
-      </Link>
+      <div className="text-white">
+        <Link
+          className={iconFormat}
+          href="https://www.facebook.com/profile.php?viewas=100000686899395&id=61550312611527"
+        >
+          <AiFillFacebook />
+          <p className={textFormat}>Facebook</p>
+        </Link>
+      </div>
+    );
+  }
+  if (type === "gigsalad") {
+    return (
+      <div className="text-white">
+        <Link
+          className={iconFormat}
+          href="https://www.gigsalad.com/ucr_aag_riverside"
+        >
+          <Image
+            className="fill-aag-red"
+            width="60"
+            height="60"
+            src="logo.svg"
+          />
+          <p className="text-xl font-light mt-4">GigSalad</p>
+        </Link>
+      </div>
     );
   }
 };
