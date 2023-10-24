cefbffaf2d40974d5f0bcb387f6dd8559ee43d47
Atharva <147217989+atharvanev@users.noreply.github.com>
2023-10-23 17:19:07 -0700

    built board component and removed extra code

--- src/app/boards/page.js
+++ src/app/boards/page.js
@@ -1,12 +1,8 @@
-import Board from "@/components/Board";
 import PageTitle from "@/components/PageTitle";
-import image1 from "public/cat.png";
-
 const Boards = () => {
   return (
     <div className="text-white">
       <PageTitle title="BOARDS" />
-      <Board image={image1} name="Evan little cat" role="Emotional Support" />
     </div>
   );
 };
