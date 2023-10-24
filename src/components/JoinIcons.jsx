de80372f39d16f475ebd73225e4cfaa47aebadf4
Jiahan Li <jcl@JiahandeMacBook-Air.local>
2023-10-23 20:32:03 -0400

    I added the JoinIcons component

---
+++ src/components/JoinIcons.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import JoinIcon from "@/components/JoinIcon";
+
+const JoinIcons = ({}) => {
+  return (
+    <div className="w-1/2 flex justify-between">
+      <JoinIcon type="gigsalad" />
+      <JoinIcon type="instagram" />
+      <JoinIcon type="facebook" />
+      <JoinIcon type="tiktok" />
+      <JoinIcon type="youtube" />
+    </div>
+  );
+};
+
+export default JoinIcons;
