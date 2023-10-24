04a8113879dde93711402fed767adb36c6ddd5a7
Atharva <147217989+atharvanev@users.noreply.github.com>
2023-10-23 17:17:57 -0700

    built board component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Image from "next/image";
+// import image2 from "../../public/cat.png";
 
-const Board = () => {
-  return <div>Board</div>;
+const Board = ({ image, role, name }) => {
+  return (
+    // <div>Board</div>
+    <div className="font-Poppins font-bold">
+      <Image
+        className="aspect-square object-cover"
+        src={image}
+        alt="Board Member"
+      />
+      <p className="text-white text-2xl	">{name}</p>
+
+      <p className="text-aag-red text-2xl	">{role}</p>
+    </div>
+  );
 };
 
 export default Board;
