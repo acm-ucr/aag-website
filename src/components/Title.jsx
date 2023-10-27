bf9dcfc84964940fc7ba3a3f96525219e236e497
Darius Mahamedi <dmaha004@ucr.edu>
2023-10-27 14:32:55 -0700

    Add title components

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Image from "next/image";
+import image1 from "../../public/fire.png";
+import image2 from "../../public/logo.svg";
+import JoinButton from "./JoinButton";
 
 const Title = () => {
-  return <div>Title</div>;
+  return (
+    <div className="relative">
+      <Image src={image1} alt="description of the image" />
+      <div className="absolute inset-x-0 top-10 flex flex-col items-center justify-start z-10 scale-150">
+        <Image src={image2} alt="description of the image" />
+        <div className="text-2xl text-white">UCR FUSION DANCE TEAM</div>
+      </div>
+      <div className="absolute inset-x-0 bottom-24 flex items-end justify-center z-10">
+        <JoinButton />
+      </div>
+    </div>
+  );
 };
 
 export default Title;
