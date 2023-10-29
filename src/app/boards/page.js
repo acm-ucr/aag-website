0c28aeb3ced1fa4274b11a9b5a8e94ad4ad1298b
Shing Hung <123037099+ysh0305@users.noreply.github.com>
2023-10-29 15:11:54 -0700

    Update page.js

--- src/app/boards/page.js
+++ src/app/boards/page.js
@@ -7,7 +7,7 @@ import { boards } from "@/data/boards";
 const Boards = () => {
   return (
     <>
-      <Row className="w-9/12 h-3/4 flex items-center justify-start">
+      <Row className="w-7/12 flex items-center justify-start">
         {boards.map((board, index) => (
           <Col key={index} xs={10} sm={2} lg={4}>
             <Board
