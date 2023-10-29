de171d8556ce4eb8f8d059709c6fadbf16b97367
Shing Hung <yhung022@ucr.edu>
2023-10-29 15:25:32 -0700

    add title

--- src/app/boards/page.js
+++ src/app/boards/page.js
@@ -3,10 +3,11 @@ import Board from "@/components/Board/";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { boards } from "@/data/boards";
-
+import PageTitle from "@/components/PageTitle";
 const Boards = () => {
   return (
     <>
+      <PageTitle title="BOARDS" />
       <Row className="w-7/12 flex items-center justify-start">
         {boards.map((board, index) => (
           <Col key={index} xs={10} sm={2} lg={4}>
--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageTitle = ({ title }) => {
   return (
-    <div className=" py-6 text-white w-full font-jost text-2xl font-bold">
+    <div className=" py-6 text-center text-white w-full font-jost text-2xl font-bold">
       {title}
     </div>
   );
