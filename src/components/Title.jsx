74d80e9054e358740e9e62006592948908663d25
Darius Mahamedi <dmaha004@ucr.edu>
2023-10-30 10:05:21 -0700

    Updated Title.jsx component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -7,12 +7,14 @@ import JoinButton from "./JoinButton";
 const Title = () => {
   return (
     <div className="relative">
-      <Image src={image1} alt="description of the image" />
-      <div className="absolute inset-x-0 top-10 flex flex-col items-center justify-start z-10 scale-150">
-        <Image src={image2} alt="description of the image" />
-        <div className="text-2xl text-white">UCR FUSION DANCE TEAM</div>
+      <Image src={image1} alt="Background" className="scale-95" />
+      <div className="absolute inset-x-0 top-20 flex flex-col items-center justify-start z-10 scale-150">
+        <Image src={image2} alt="Logo" width="200" />
+        <div className="text-xl text-white font-bold">
+          FUSION DANCE TEAM AT UCR
+        </div>
       </div>
-      <div className="absolute inset-x-0 bottom-24 flex items-end justify-center z-10">
+      <div className="absolute inset-x-0 bottom-20 flex items-end justify-center z-10">
         <JoinButton />
       </div>
     </div>
