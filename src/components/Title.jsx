c81231d2aacca7feaa359705501f299895da4f66
Shing Hung <123037099+ysh0305@users.noreply.github.com>
2023-10-31 11:44:11 -0700

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -8,10 +8,10 @@ const Title = () => {
   return (
     <div className="relative">
       <Image src={image1} alt="Background" className="scale-95" />
-      <div className="absolute inset-x-0 top-20 flex flex-col items-center justify-start z-10 scale-150">
+      <div className="absolute inset-x-0 top-20 flex flex-col items-center justify-start z-10 scale-110">
         <Image src={image2} alt="Logo" width="200" />
-        <div className="text-xl text-white font-bold">
-          FUSION DANCE TEAM AT UCR
+        <div className="text-3xl mt-1 text-white font-bold">
+          Fusion Dance Team at UCR
         </div>
       </div>
       <div className="absolute inset-x-0 bottom-20 flex items-end justify-center z-10">
