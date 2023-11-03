2dea4463a31882502176ef35d4deec8fc9744834
Shing Hung <123037099+ysh0305@users.noreply.github.com>
2023-11-02 19:35:23 -0700

    Update ImageBars.jsx

--- src/components/ImageBars.jsx
+++ src/components/ImageBars.jsx
@@ -13,30 +13,38 @@ const ImageBars = () => {
       <Image
         src={redbar}
         alt="red stripes"
-        className="self-end w-1/4 blur-sm"
+        className="self-end w-1/6 md:w-1/5 blur-sm"
       />
       <Image
         src={redbar}
         alt="red stripes"
-        className="rotate-180 self-start w-1/4 blur-sm"
+        className="rotate-180 self-start w-1/6 md:w-1/5 blur-sm"
       />
       <div className="flex my-[8%] lg:scale-110 object-contain absolute z-10 w-3/4">
         <Image
           src={image1}
           alt="Blue Dancer"
-          className="px-1.5 mb-[7%] mt-[3%]"
+          className="px-1.5 mb-[7%] mt-[3%] w-1/5"
         />
-        <Image src={image4} alt="3 white pic" className="px-1.5 mt-[10%]" />
+        <Image
+          src={image4}
+          alt="3 white pic"
+          className="px-1.5 mt-[10%] w-1/5"
+        />
         <Image
           src={image2}
           alt="Yellow w/purple background"
-          className="px-1.5 mb-[4%] mt-[6%]"
+          className="px-1.5 mb-[4%] mt-[6%] w-1/5"
         />
-        <Image src={image5} alt="1 white pic" className="px-1.5 mb-[11%]" />
+        <Image
+          src={image5}
+          alt="1 white pic"
+          className="px-1.5 mb-[11%] w-1/5"
+        />
         <Image
           src={image3}
           alt="Red Background"
-          className="px-1.5 mb-[2%] mt-[8%]"
+          className="px-1.5 mb-[2%] mt-[8%] w-1/5"
         />
       </div>
     </div>
