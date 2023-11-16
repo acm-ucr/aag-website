f2d1cb374f70dcab4a0d73a7ba1d7f9496162054
Shing Hung <yhung022@ucr.edu>
2023-11-16 10:48:36 -0800

    react-player

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,10 +2,10 @@ import React from "react";
 import PageTitle from "@/components/PageTitle";
 import Image from "next/image";
 import fire from "public/fire.png";
-
+import ReactPlayer from "react-player";
 const About = () => {
   return (
-    <>
+    <div>
       <title>AAG | About</title>
       <Image
         src={fire}
@@ -13,7 +13,8 @@ const About = () => {
         className=" -translate-y-[7%] object-contain rotate-180 absolute top-0"
       />
       <PageTitle title="ABOUT" />
-    </>
+      <ReactPlayer url="https://youtu.be/W-9_lIJpM2I" />
+    </div>
   );
 };
 
