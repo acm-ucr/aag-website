8ad064f1b1108c7f47bfbbb098bc6d59a6960c7c
low <akilan.purushothaman@gmail.com>
2023-11-16 20:49:30 -0800

    minor fixes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,12 +4,13 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { FaBars } from "react-icons/fa";
 import Link from "next/link";
+import Image from "next/image";
 const NavBar = () => {
   const [selected, setSelected] = useState(
     typeof window !== "undefined" ? window.location.pathname : ""
   );
   const textFormat =
-    "transition ease-in-out hover:!text-aag-red font-light mb-0 px-3 text-white text-xl whitespace-nowrap";
+    "transition ease-in-out hover:!text-aag-red font-light mb-0 px-3 text-xl whitespace-nowrap";
   return (
     <Navbar
       collapseOnSelect
@@ -17,11 +18,21 @@ const NavBar = () => {
       fixed="top"
       className=" justify-end w-full md:min-h-[5vh] p-0 !bg-black/50 backdrop-blur-sm"
     >
+      <Link
+        className="flex px-3 space-x-3"
+        onClick={() => {
+          setSelected("/");
+        }}
+        href="/"
+      >
+        <Image width="40" height="40" src="logo.svg" />
+        <Image width="100" height="40" src="aag.svg" />
+      </Link>
       <Navbar.Toggle
         className="list-unstyled !text-transparent border-0"
         aria-controls="basic-navbar-nav"
       >
-        <FaBars className="text-white text-4xl" />
+        <FaBars className="text-4xl" />
       </Navbar.Toggle>
       <Navbar.Collapse className="items-center justify-end md:mr-8 flex">
         <Nav className="flex items-center w-full md:w-auto">
@@ -32,7 +43,7 @@ const NavBar = () => {
               setSelected("/about");
             }}
             className={`${textFormat} ${
-              selected === "/about" ? "underline" : "no-underline"
+              selected === "/about" ? "!text-aag-red" : "text-white"
             }`}
           >
             ABOUT
@@ -41,13 +52,13 @@ const NavBar = () => {
             as={Link}
             href="/board"
             onClick={() => {
-              setSelected("/boards");
+              setSelected("/board");
             }}
             className={`${textFormat} ${
-              selected === "/boards" ? "underline" : "no-underline"
+              selected === "/board" ? "!text-aag-red" : "text-white"
             }`}
           >
-            BOARDS
+            BOARD
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
