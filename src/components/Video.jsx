03cab83fabc9f3e5f415f1e6ee52f88010da7257
Shing Hung <yhung022@ucr.edu>
2023-11-22 22:19:16 -0800

    compress png and delete row/col

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,12 +3,11 @@ import React from "react";
 const Video = ({ link, title }) => {
   return (
     <iframe
-      className="w-3/4 aspect-video"
+      className=" aspect-video md:w-7/12 w-10/12 my-3"
       src={link}
       title={title}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowfullscreen
-      height="800"
     />
   );
 };
