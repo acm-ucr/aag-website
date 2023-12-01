910c6bd7948015e5691e8a2ab8dd2c5ac2001914
Jiahan Li <jcl@JiahandeMacBook-Air.local>
2023-11-30 22:06:03 -0500

    I mapped the videos.

--- src/components/JoinButton.jsx
+++ src/components/JoinButton.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 const JoinButton = () => {
   return (
     <Link
+      target="_blank"
       className="transition ease-in-out bg-gradient-to-r from-aag-red to-aag-orange text-3xl px-5 py-3 text-white font-bold md:rounded-none rounded-sm 
     hover:opacity-80 active:opacity-60"
       href=" https://docs.google.com/forms/d/e/1FAIpQLSd-83k_aUvm_-HMH4LZRpsSVBMDFK8yf3uRygBaFVJHRZQqnw/viewform"
